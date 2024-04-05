8532f6c67773d62bf086ac87d7b901f984e203fc
vaidik24 <vaidik24pandya@gmail.com>
2024-04-05 15:31:27 +0530

    corrected the login button

--- Client/FrontEnd/src/components/Login.jsx
+++ Client/FrontEnd/src/components/Login.jsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=860060f6eb7743a2b87dcead95d611c3&response_type=code&redirect_uri=http://localhost:3000/auth&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private";
 
-
-
 function Login() {
   return (
     <>
@@ -23,9 +21,9 @@ function Login() {
         </div>
         <div className="input-field">
           <h4>Spotify Login</h4>
-          <button className="spotify-login-button">
-            <Link to={AUTH_URL}>Go to Spotify Login</Link>
-          </button>
+          <Link to={AUTH_URL} className="spotify-login-button">
+            Go to Spotify Login
+          </Link>
         </div>
       </div>
     </>
