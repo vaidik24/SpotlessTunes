21ca7abc3527408a6424da9d46a6b5defcf8d479
rahil <rahilganatra@gmail.com>
2024-04-06 16:44:16 +0530

    setting context of access_token to its children component

--- Client/FrontEnd/src/App.jsx
+++ Client/FrontEnd/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 import Login from "./components/Login.jsx";
-import Dashboard from "./components/Dashboard.jsx";
+import RemoveDuplicates from "./components/RemoveDuplicates.jsx";
 import AuthCodeHandler from "./components/AuthCodeHandler.jsx";
 import NavBar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import { AccessTokenProvider } from "./components/AccessTokenContext.jsx";
+import Statistics from "./components/Statistics.jsx";
 function App() {
   // const code = new URLSearchParams(window.location.search).get("code");
   // const accessToken = useAuth(code);
@@ -11,20 +14,24 @@ function App() {
   return (
     <>
       <Router>
+        <AccessTokenProvider>
         <NavBar />
         <Routes>
           {/*<Route*/}
           {/*  path="/"*/}
-          {/*  element={access_token !== "" ? <Dashboard accessToken={access_token} /> : <Login />}*/}
+          {/*  element={access_token !== "" ? <RemoveDuplicates accessToken={access_token} /> : <Login />}*/}
           {/*/>*/}
           {/*<Route*/}
           {/*    path="/auth"*/}
           {/*    element={ <AuthCodeHandler/> }*/}
           {/*/>*/}
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/remove-duplicates" element={<RemoveDuplicates />} />
           <Route path="/auth" element={<AuthCodeHandler />} />
+          <Route path="/stats" element={<Statistics />} />
         </Routes>
+        </AccessTokenProvider>
       </Router>
     </>
   );
