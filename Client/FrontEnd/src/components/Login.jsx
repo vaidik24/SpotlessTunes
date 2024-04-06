b6820cba59b4914c93d59f924340c00411b74d96
rahil <rahilganatra@gmail.com>
2024-04-06 16:45:49 +0530

    added user-top-read scope for getting user stats

--- Client/FrontEnd/src/components/Login.jsx
+++ Client/FrontEnd/src/components/Login.jsx
@@ -2,7 +2,7 @@ import "../styles/login.css";
 import { Link } from "react-router-dom";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=860060f6eb7743a2b87dcead95d611c3&response_type=code&redirect_uri=http://localhost:3000/auth&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private";
+  "https://accounts.spotify.com/authorize?client_id=860060f6eb7743a2b87dcead95d611c3&response_type=code&redirect_uri=http://localhost:3000/auth&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-top-read";
 
 function Login() {
   return (
