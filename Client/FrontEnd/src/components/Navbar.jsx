3ecdfd633cb59183e129921ca221e2c49e03c8a2
rahil <rahilganatra@gmail.com>
2024-04-09 21:44:28 +0530

    changed home button route to /home instead of /

--- Client/FrontEnd/src/components/Navbar.jsx
+++ Client/FrontEnd/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="navbar-item">
-          <Link to="/" className="navbar-link">
+          <Link to="/home" className="navbar-link">
             Home
           </Link>
         </li>
